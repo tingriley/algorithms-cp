e34c944cf6005b20279630ffeed3aad142ab2757
tingriley <handsomepot@gmail.com>
2022-05-29 09:15:01 +0800

    add find prime

--- Prime.cpp
+++ Prime.cpp
@@ -1,5 +1,4 @@
 //Sieve of Eratosthenes
-
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -8,6 +7,7 @@ int main()
 	int n;
 	vector<bool> is_prime(n+1, true);
 	is_prime[0] = is_prime[1] = false;
+
 	for (int i = 2; i <= n; i++) {
 	    if (is_prime[i] && (long long)i * i <= n) {
 	        for (int j = i * i; j <= n; j += i)
